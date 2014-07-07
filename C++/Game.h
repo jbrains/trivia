3e14244bf3d897df11306f0a3595c4847b59951f
Tamas Szirbucz <szirbucz.tamas@gmail.com>
2014-07-07 20:59:54 +0200

    [refactoring] Style has been refactored.

--- C++/Game.h
+++ C++/Game.h
@@ -1,48 +1,56 @@
+#ifndef GAME_H_
+#define GAME_H_
+
 #include <iostream>
 #include <list>
 #include <vector>
 using namespace std;
 
-#ifndef GAME_H_
-#define GAME_H_
-
-class Game{
-
-		private:
-			vector<string> players;
-
-			int places[6];
-			int purses[6];
-
-			bool inPenaltyBox[6];
-
-			list<string> popQuestions;
-			list<string> scienceQuestions;
-			list<string> sportsQuestions;
-			list<string> rockQuestions;
-
-			int currentPlayer;
-			bool isGettingOutOfPenaltyBox;
-
+class Game
+{
 public:
-	Game();
-	string createRockQuestion(int index);
-	bool isPlayable();
-	bool add(string playerName);
+    Game ();
 
-	int howManyPlayers();
-	void roll(int roll);
+    string createRockQuestion (int index);
 
-	private:
-		void askQuestion();
-		string currentCategory();
+    bool isPlayable ();
 
-				public:
-					bool wasCorrectlyAnswered();
-					bool wrongAnswer();
+    bool add (string playerName);
+
+    int howManyPlayers ();
+
+    void roll (int roll);
+
+    bool wasCorrectlyAnswered ();
+
+    bool wrongAnswer ();
 
 private:
-	bool didPlayerWin();
+    vector<string> players;
+
+    int places[6];
+
+    int purses[6];
+
+    bool inPenaltyBox[6];
+
+    list<string> popQuestions;
+
+    list<string> scienceQuestions;
+
+    list<string> sportsQuestions;
+
+    list<string> rockQuestions;
+
+    int currentPlayer;
+
+    bool isGettingOutOfPenaltyBox;
+
+    void askQuestion ();
+
+    string currentCategory ();
+
+    bool didPlayerWin ();
 };
 
 #endif /* GAME_H_ */
--- C++/GameRunner.cpp
+++ C++/GameRunner.cpp
@@ -1,29 +1,30 @@
 ﻿#include <stdlib.h>
+
 #include "Game.h"
 
 static bool notAWinner;
 
 int main()
 {
-	Game aGame;
+    Game aGame;
 
-	aGame.add("Chet");
-	aGame.add("Pat");
-	aGame.add("Sue");
+    aGame.add("Chet");
+    aGame.add("Pat");
+    aGame.add("Sue");
 
-	do
-	{
+    do
+    {
+        aGame.roll(rand() % 5 + 1);
 
-		aGame.roll(rand() % 5 + 1);
+        if (rand() % 9 == 7)
+        {
+            notAWinner = aGame.wrongAnswer();
+        }
+        else
+        {
+            notAWinner = aGame.wasCorrectlyAnswered();
+        }
+    } while (notAWinner);
 
-		if (rand() % 9 == 7)
-		{
-			notAWinner = aGame.wrongAnswer();
-		}
-		else
-		{
-			notAWinner = aGame.wasCorrectlyAnswered();
-		}
-	} while (notAWinner);
-
+    return 0;
 }
