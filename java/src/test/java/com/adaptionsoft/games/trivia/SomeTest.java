12dbb54427a1d52e512cf34ed80cbbed91d109f8
Marco Sabatini <marco.sabatini@external-rumbo.com>
2017-12-27 10:56:18 +0100

    Try to do the golden master test. Now I have some trouble with the random behaviour

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -1,13 +1,40 @@
 package com.adaptionsoft.games.trivia;
 
-import static org.junit.Assert.*;
-
+import com.adaptionsoft.games.trivia.runner.GameRunner;
+import org.junit.Before;
 import org.junit.Test;
 
-public class SomeTest {
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.net.URI;
 
-	@Test
-	public void true_is_true() throws Exception {
-		assertTrue(false);
-	}
+import static java.nio.file.Files.readAllBytes;
+import static java.nio.file.Paths.get;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SomeTest
+{
+
+  @Before
+  public void setUp() throws Exception
+  {
+    System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream("./target/test-classes/Run.txt")), true));
+  }
+
+  @Test
+  public void myfirstTest() throws Exception
+  {
+
+    GameRunner runner = new GameRunner();
+    runner.main(null);
+
+    String goldenMaster = new String(readAllBytes(get(new URI("file:///Users/msabatini/work/trivia/java/src/test/resources/GoldenMaster.txt"))));
+    String theRun = new String(readAllBytes(get(new URI("file:///Users/msabatini/work/trivia/java/target/test-classes/Run.txt"))));
+
+    assertThat(goldenMaster,is(theRun));
+
+
+  }
 }
