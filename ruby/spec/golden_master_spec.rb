af558844192897e15148e033e55ed16765926aef
Victoria Schiffer <vschiffer@seek.com.au>
2018-06-30 22:01:08 +1000

    fixed approval dependencies and added start of seeded rand methods

--- ruby/spec/golden_master_spec.rb
+++ ruby/spec/golden_master_spec.rb
@@ -1,7 +1,47 @@
 require 'spec_helper'
+require 'approvals/rspec'
+require 'ugly_trivia/game'
+
+describe "spec test" do
+  it "should pass" do
+    expect(true).to eq(true)
+  end
+end
 
 describe "golden master test" do
-  it "should pass" do
-    expect(false).to eq(true)
+  it "should record and verify golden master" do
+  	play(1040)
+  	verify :format => :txt do
+  		"text"
+  	end
   end
+end
+
+def play(seed)
+
+	puts "seed: #{seed}"
+	srand(seed)
+	puts rand(seed)
+	for i in 1..10
+		puts rand i
+	end
+
+	# not_a_winner = false
+
+	# game = UglyTrivia::Game.new
+	# game.add 'Chet'
+	# game.add 'Pat'
+	# game.add 'Sue'
+
+	# begin
+	  
+	#   game.roll(rand(5) + 1)
+	  
+	#   if rand(9) == 7
+	#     not_a_winner = game.wrong_answer
+	#   else
+	#     not_a_winner = game.was_correctly_answered
+	#   end
+	  
+	# end while not_a_winner
 end
