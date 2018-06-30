65f2d806c45264ddc3a661daf9de0c6b6c7cf7d0
Victoria Schiffer <vschiffer@seek.com.au>
2018-06-30 22:41:44 +1000

    added first shot at golden master

--- ruby/spec/golden_master_spec.rb
+++ ruby/spec/golden_master_spec.rb
@@ -10,38 +10,62 @@ end
 
 describe "golden master test" do
   it "should record and verify golden master" do
-  	play(1040)
+  		old_stdout = $stdout
+  		File.open('spec/fixtures/approvals/golden_master_test/runtime_output.txt', 'w') do |fo|
+	  		$stdout = fo
+	  		for i in 1..6
+			  	play(26469*i)
+			  	puts('**************************')
+			  	puts('==========================')
+			  	puts('**************************')
+			  	puts('==========================')
+				end
+			end
+			
+			file = File.open($stdout)
+			text_to_verify = ""
+			file.each {|line|
+			  text_to_verify << line
+			}
+
+			$stdout = old_stdout
   	verify :format => :txt do
-  		"text"
+  		text_to_verify
   	end
   end
 end
 
-def play(seed)
 
+def print_seed(seed)
 	puts "seed: #{seed}"
 	srand(seed)
 	puts rand(seed)
 	for i in 1..10
 		puts rand i
 	end
+end
 
-	# not_a_winner = false
 
-	# game = UglyTrivia::Game.new
-	# game.add 'Chet'
-	# game.add 'Pat'
-	# game.add 'Sue'
+def play(seed)
 
-	# begin
+	# print_seed(seed)
+
+	not_a_winner = false
+
+	game = UglyTrivia::Game.new
+	game.add 'Chet'
+	game.add 'Pat'
+	game.add 'Sue'
+
+	begin
+	  game.roll(rand(5) + 1)
 	  
-	#   game.roll(rand(5) + 1)
+	  if rand(9) == 7
+	    not_a_winner = game.wrong_answer
+	  else
+	    not_a_winner = game.was_correctly_answered
+	  end
 	  
-	#   if rand(9) == 7
-	#     not_a_winner = game.wrong_answer
-	#   else
-	#     not_a_winner = game.was_correctly_answered
-	#   end
-	  
-	# end while not_a_winner
+	end while not_a_winner
+
 end
