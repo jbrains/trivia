64dcd781b13321f7e2a83fa79a6a3336c4341736
BGyore <90140002+BGyore@users.noreply.github.com>
2021-09-05 19:16:37 +0200

    Update Game.h
    I have added template, delete the unnecessary includes, correct the issues in the class, delete the unnecessary return values and so on.

--- C++/Game.h
+++ C++/Game.h
@@ -1,48 +1,93 @@
+/**\file
+ *  FILE DESCRIPTION
+ *  --------------------------------------------------------------------------
+ *  File name: Game.h
+ *
+ *  Short Description:
+ *  Declaration of Game class.
+ */
+
+/*============================================================================*\
+ * PREPROCESSOR DIRECTIVES
+\*============================================================================*/
+
+/* INCLUDE DIRECTIVES FOR STANDARD HEADERS -----------------------------------*/
+
 #include <iostream>
 #include <list>
 #include <vector>
-using namespace std;
+
+/* INCLUDE DIRECTIVES FOR OTHER HEADERS --------------------------------------*/
+
+/* HEADER GUARD --------------------------------------------------------------*/
 
 #ifndef GAME_H_
 #define GAME_H_
 
+/*============================================================================*\
+ * NAMESPACES
+\*============================================================================*/
+
+namespace game {
+
+/*============================================================================*\
+ * FILE SCOPE DECLARATIONS AND DEFINITIONS
+\*============================================================================*/
+
+/* CONSTANTS -----------------------------------------------------------------*/
+
+/* ENUMS ---------------------------------------------------------------------*/
+
+/* STRUCTS -------------------------------------------------------------------*/
+
+/* OTHER TYPEDEFS ------------------------------------------------------------*/
+
+/*============================================================================*\
+ * MEMBER FUNCTION DECLARATIONS
+\*============================================================================*/
+
 class Game{
-
-		private:
-			vector<string> players;
-
-			int places[6];
-			int purses[6];
-
-			bool inPenaltyBox[6];
-
-			list<string> popQuestions;
-			list<string> scienceQuestions;
-			list<string> sportsQuestions;
-			list<string> rockQuestions;
-
-			unsigned int currentPlayer;
-			bool isGettingOutOfPenaltyBox;
-
 public:
 	Game();
-	string createRockQuestion(int index);
-	bool isPlayable();
-	bool add(string playerName);
+	~Game();
 
-	int howManyPlayers();
-	void roll(int roll);
-
-	private:
-		void askQuestion();
-		string currentCategory();
-
-				public:
-					bool wasCorrectlyAnswered();
-					bool wrongAnswer();
+	void add(std::string playerName);
+	void roll(unsigned int roll);
+	bool wasCorrectlyAnswered();
+    void wrongAnswer();
 
 private:
+    void createQuestions();
+    bool isPlayable();
+    unsigned int howManyPlayers();
+    void askQuestion();
+    std::string currentCategory();
 	bool didPlayerWin();
+
+	std::vector<std::string> players;
+
+    unsigned int places[6U];
+    unsigned int purses[6U];
+
+    bool inPenaltyBox[6U];
+
+    std::list<std::string> popQuestions;
+    std::list<std::string> scienceQuestions;
+    std::list<std::string> sportsQuestions;
+    std::list<std::string> rockQuestions;
+
+    unsigned int currentPlayer;
+    bool isGettingOutOfPenaltyBox;
+
+protected:
 };
 
+} // game namespace
+
+/* END OF HEADER GUARD -------------------------------------------------------*/
+
 #endif /* GAME_H_ */
+
+/*============================================================================*\
+ * END OF FILE
+\*============================================================================*/
