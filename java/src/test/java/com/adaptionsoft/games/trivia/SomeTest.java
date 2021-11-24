d23abbaecaf2cda320b9b758ebe0dd66a6d8552c
Guido Dechamps <guido@tripled.io>
2021-11-24 10:25:42 +0100

    Resolved code remarks

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -3,12 +3,12 @@ package com.adaptionsoft.games.trivia;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SomeTest {
 
     @Test
     public void true_is_true() throws Exception {
-        fail();
+        assertTrue(false);
     }
 }
