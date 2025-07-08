ca59e511c95c82a55f65337cbea83ae25d530c09
Romain <romain.code@pm.me>
2025-07-08 09:59:28 +0200

    Add ArrayList in arrayListPrinter

--- java/src/test/java/com/adaptionsoft/games/trivia/runner/ArrayListPrinter.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/runner/ArrayListPrinter.java
@@ -2,10 +2,15 @@ package com.adaptionsoft.games.trivia.runner;
 
 import com.adaptionsoft.games.uglytrivia.Printer;
 
+import java.util.ArrayList;
+
 public class ArrayListPrinter implements Printer {
+
+    ArrayList<Object> list = new ArrayList<>();
 
     @Override
     public void println(Object message) {
         System.out.println(message);
+        list.add(message);
     }
 }
