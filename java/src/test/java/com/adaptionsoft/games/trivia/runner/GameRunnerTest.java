f5beb995532790914f98681973280c7cf3dafbdf
Romain <romain.code@pm.me>
2025-07-08 09:41:15 +0200

    introduce seed to make the test repetable

---
+++ java/src/test/java/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
@@ -0,0 +1,16 @@
+package com.adaptionsoft.games.trivia.runner;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameRunnerTest {
+
+    @Test
+    public void test_xxx() {
+        GameRunner.runGame(new Random(0));
+    }
+
+}
