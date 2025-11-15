4fe5fe9a2007d0994ca6bf1336921ca88f271434
Jakub Wąsik <jakubwasik@Jakubs-MacBook-Pro.local>
2025-11-15 14:48:10 +0100

    Session 4 - NodeJS: Setup - Golden Record

--- nodejs/src/game.js
+++ nodejs/src/game.js
@@ -160,22 +160,6 @@ var Game = function () {
   };
 };
 
-var notAWinner = false;
-
-var game = new Game();
-
-game.add("Chet");
-game.add("Pat");
-game.add("Sue");
-
-do {
-  game.roll(Math.floor(Math.random() * 6) + 1);
-
-  if (Math.floor(Math.random() * 10) == 7) {
-    notAWinner = game.wrongAnswer();
-  } else {
-    notAWinner = game.wasCorrectlyAnswered();
-  }
-} while (notAWinner);
+require("seedrandom")("42", { global: true });
 
 module.exports = Game;
--- nodejs/src/game.spec.js
+++ nodejs/src/game.spec.js
@@ -1,16 +1,28 @@
 const Should = require("should");
 const Game = require("./game.js");
+const fs = require("node:fs");
 
-describe("The test environment", function () {
-  it("should pass", function () {
-    true.should.equal(true);
+class Snapshot {
+  static get = (output = "output.md", output_test = "output_test.md") => [
+    fs.readFileSync(output, { encoding: "utf-8" }),
+    fs.readFileSync(output_test, { encoding: "utf-8" }),
+  ];
+}
+
+// describe("The test environment", function () {
+//   it("should pass", function () {
+//     true.should.equal(true);
+//   });
+
+//   it("should access game", function () {
+//     Should(Game).not.equal(undefined);
+//   });
+// });
+
+describe("Golden Record snapshots", function () {
+  it("should match", () => {
+    const [expected, actual] = Snapshot.get();
+
+    actual.should.equal(expected);
   });
-
-  it("should access game", function () {
-    Should(Game).not.equal(undefined);
-  });
-});
-
-describe("Your specs...", function () {
-  // it ...
 });
